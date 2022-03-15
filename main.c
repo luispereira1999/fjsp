775a46cea2667b0fbcb7d48b41d6e7cbfef6964d
Luís Pereira <luispereira1999@gmail.com>
2022-03-15 22:40:27 +0000

    Descrição dos ficheiros

---
+++ main.c
@@ -0,0 +1,6 @@
+/*
+Descrição:           Ficheiro principal da aplicação
+Desenvolvedor(es):   Luís Pereira (18446)
+Criação:             14/03/2022
+Última atualização:  31/03/20221
+*/
---
+++ stage1.c
@@ -0,0 +1,6 @@
+/*
+Descrição:           Ficheiro com todas as funções e procedimentos relativos à fase 1 da aplicação
+Desenvolvedor(es):   Luís Pereira (18446)
+Criação:             14/03/2022
+Última atualização:  31/03/20221
+*/
