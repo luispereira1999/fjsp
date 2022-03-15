a2647150f16364488f2f2e73d9d85bb06105a7a3
Luís Pereira <luispereira1999@gmail.com>
2022-03-15 22:41:50 +0000

    Definir representações e estruturas

--- stage1.h
+++ stage1.h
@@ -4,3 +4,44 @@ Desenvolvedor(es):   Luís Pereira (18446)
 Criação:             14/03/2022
 Última atualização:  31/03/20221
 */
+
+#ifndef STAGE1_H
+
+#pragma region constantes
+
+#define HEADER_H
+
+#pragma endregion
+
+#pragma region representações e estruturas
+
+typedef enum bool
+{
+   false = 0,
+   true = 1
+} bool;
+
+typedef struct Machine
+{
+   int id;
+   int time; // quantidade de unidades de tempo necessária para a realização da operação
+   struct Machine *next;
+} Machine;
+
+typedef struct Operation
+{
+   int id;
+   struct Machine *machines;
+   struct Operation *next;
+} Operation;
+
+typedef struct Job
+{
+   int id;
+   struct Operation *operations;
+   struct Job *next;
+} Job;
+
+#pragma endregion
+
+#endif
